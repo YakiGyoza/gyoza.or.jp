77d2a177bbf44645584cd57248462963d53e13fc
Chikara Onodera <chikara@shikake.com>
2019-02-05 18:22:37 +0900

    OGP image追加

--- static/sw.js
+++ static/sw.js
@@ -2,28 +2,28 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/4412fa2674079e5b238b.js",
-    "revision": "fa45cb73162145d4a980a48a9218b219"
-  },
-  {
-    "url": "/_nuxt/7554581d53994d38f06e.js",
-    "revision": "876aa2b088d6068d133704bc201fc3c8"
-  },
-  {
     "url": "/_nuxt/7c900f47ba77635e06b8.js",
     "revision": "8355e734b02008fa4fdb16847a3d0e94"
+  },
+  {
+    "url": "/_nuxt/9289d31d3f09ed8ae416.js",
+    "revision": "a6b9a012d7be44e20c137c82edf63046"
   },
   {
     "url": "/_nuxt/96a70630545f544c28e1.js",
     "revision": "43ab2d3e67370d0589cb9c862e350b1e"
   },
   {
+    "url": "/_nuxt/99ae7ff3fa8361cee238.js",
+    "revision": "104873e4d8579911bf50f58458682cd4"
+  },
+  {
     "url": "/_nuxt/9e251bc63a23d3d8c2c1.js",
     "revision": "1e91b12f52edc18d151bc83acf98839c"
   },
   {
-    "url": "/_nuxt/b949616fde6ca856d6fe.js",
-    "revision": "b3dc7b5839941d5e494fe2b0c2732717"
+    "url": "/_nuxt/acd71762f31f86eef9b5.js",
+    "revision": "54a074d255294db32e25e5712e77eada"
   },
   {
     "url": "/_nuxt/d91c3ec93b9cb0c4daba.js",
